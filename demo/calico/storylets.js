f0ff1852d802561d62f5f5087588a5414af71a12
Florian Cargoët <florian.cargoet@gmail.com>
2023-11-24 23:26:31 +0100

    0.3.0

--- demo/calico/storylets.js
+++ demo/calico/storylets.js
@@ -348,7 +348,7 @@ const credits = {
   name: "Storylets",
   author: "Florian Cargo\xEBt",
   // @ts-expect-error Injected by rollup
-  version: "0.2.4",
+  version: "0.3.0",
   description: "Storylets",
   licences: {
     self: "2023"
